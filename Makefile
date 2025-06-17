3d1025306a9664f5f70647281262ed5382518ab9
Egor00f <velikiydolbayeb@gmail.com>
2025-06-17 23:07:23 +0500

    fix __call && add makefile

---
+++ Makefile
@@ -0,0 +1,27 @@
+LIBNAME = cooldownlib
+
+PACKAGE_NAME = $(LIBNAME).zip
+
+CP = cp -rf
+MV = mv -f
+RM = rm -rf
+
+./build: 
+	mkdir build
+	
+configure: ./build wally.toml src/*
+	$(CP) src/* build/
+	$(MV) build/$(LIBNAME).lua build/init.lua
+	$(CP) wally.toml build/
+
+package: configure
+	wally package --output $(PACKAGE_NAME) --project-path build
+
+publish: configure
+	wally publish
+
+lint:
+	selene src/ tests/
+
+clean: 
+	$(RM) -rf build $(PACKAGE_NAME)
--- src/cooldown.lua
+++ src/cooldown.lua
@@ -1,19 +1,36 @@
+--[[
+	CooldownLib
+]]
 local cooldown = {}
 
 export type Cooldown<ArgsType, RetType> = {
-	func: (... ArgsType) -> any,
+	--[[
+		function that wil be called
+	]]
+	func: (...ArgsType) -> any,
+
+	--[[
+		Can run function right now
+	]]
 	CanRun: boolean,
+
+	--[[
+		cooldown time
+	]]
 	timeout: number,
+
+	--[[
+		
+	]]
 	CallEvent: BindableEvent,
 	counterThread: thread,
 
-	Call: (self: Cooldown<ArgsType, RetType>, ... ArgsType) -> RetType?,
-	__call: (self: Cooldown<ArgsType, RetType>, ... ArgsType) -> RetType?,
-	Destroy: (self: Cooldown<ArgsType, RetType>) -> nil
+	Call: (self: Cooldown<ArgsType, RetType>, ...ArgsType) -> RetType?,
+
+	Destroy: (self: Cooldown<ArgsType, RetType>) -> nil,
 }
 
 function cooldown.Destroy<ArgsType, RetType>(self: Cooldown<ArgsType, RetType>)
-	task.cancel(self.counterThread)
 	self.CallEvent:Destroy()
 	table.clear(self)
 end
@@ -27,28 +44,17 @@ function cooldown.Call<ArgsType, RetType>(self: Cooldown<ArgsType, RetType>, ...
 	end
 end
 
-function cooldown.new<ArgsType, RetType>(timeout: number, func: (... ArgsType) -> RetType): Cooldown<ArgsType, RetType>
-
-	local self: Cooldown<ArgsType, RetType> 
-	
-	self = {
+function cooldown.new<ArgsType, RetType>(timeout: number, func: (...ArgsType) -> RetType): Cooldown<ArgsType, RetType>
+	local self = {
 		CanRun = true,
 		timeout = timeout,
 		func = func,
 		Call = cooldown.Call,
-		__call =  cooldown.Call,
 		Destroy = cooldown.Destroy,
-		CallEvent = Instance.new("BindableEvent")
+		CallEvent = Instance.new("BindableEvent"),
 	}
-	
-	self.counterThread = task.spawn(function()			
-		while true do
-			self.CanRun = true
-			self.CallEvent.Event:Wait()
-			self.CanRun = false
-			wait(self.timeout)
-		end
-	end)
+
+	setmetatable(self, { __call = cooldown.Call })
 
 	return self
 end
