afa2dae54528c4fb20567f04e40f0d3b0129ab1e
Egor00f <velikiydolbayeb@gmail.com>
2025-08-11 17:28:05 +0500

    return back counterThread

--- src/cooldown.lua
+++ src/cooldown.lua
@@ -25,6 +25,10 @@ type CooldownStruct<RetType, ArgsType...> = {
 		
 	]]
 	CallEvent: BindableEvent,
+
+	--[[
+	
+	]]
 	counterThread: thread?,
 }
 
@@ -35,11 +39,18 @@ export type Cooldown<RetType, ArgsType...> = typeof(setmetatable({} :: CooldownS
 	end,
 }))
 
-function cooldown.Destroy<ArgsType, RetType>(self: Cooldown<ArgsType, RetType> & {})
+--[[
+	Destroy
+]]
+function cooldown.Destroy<RetType, ArgsType...>(self: Cooldown<RetType, ArgsType...>)
+	self:StopWatch()
 	self.CallEvent:Destroy()
 	table.clear(self)
 end
 
+--[[
+	Call
+]]
 function cooldown.Call<RetType, ArgsType...>(self: Cooldown<RetType, ArgsType...>, ...: ArgsType...): RetType?
 	if self.CanRun == true then
 		self.CallEvent:Fire()
@@ -49,6 +60,35 @@ function cooldown.Call<RetType, ArgsType...>(self: Cooldown<RetType, ArgsType...
 	end
 end
 
+function cooldown.StartWatch<RetType, ArgsType...>(self: Cooldown<RetType, ArgsType...>)
+	if self.counterThread then
+		self.counterThread = task.spawn(function()
+			while true do
+				self.CanRun = true
+
+				self.CallEvent.Event:Wait()
+
+				self.CanRun = false
+
+				wait(self.timeout)
+			end
+		end)
+	else
+		warn("counterThread already started")
+	end
+end
+
+function cooldown.StopWatch<RetType, ArgsType...>(self: Cooldown<RetType, ArgsType...>)
+	if self.counterThread then
+		task.cancel(self.counterThread)
+	else
+		warn("watch don't started yet")
+	end
+end
+
+--[[
+	Constructor
+]]
 function cooldown.new<RetType, ArgsType...>(timeout: number, func: (ArgsType...) -> RetType): Cooldown<RetType, ArgsType...>
 	local self: CooldownStruct<RetType, ArgsType...> = {
 		CanRun = true,
@@ -61,6 +101,8 @@ function cooldown.new<RetType, ArgsType...>(timeout: number, func: (ArgsType...)
 		__index = cooldown,
 		__call = cooldown.Call :: <RetType, ArgsType...>(self: typeof(setmetatable({}, {})), ArgsType...) -> RetType?,
 	})
+
+	self:StartWatch()
 
 	return self
 end
